95ff5b50dd46c1dacaffdb59e2013e7078afe807
SkitFactory <31624504+SkitFactory@users.noreply.github.com>
2017-09-28 17:44:46 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,54 @@
-function setup() { 
-	createCanvas(windowWidth, windowHeight); 
-	background(255,0,0);
-}
+var quantity = 500;
+var xPosition = new Array(quantity);
+var yPosition = new Array(quantity);
+var flakeSize = new Array(quantity);
+var direction = new Array(quantity);
+var minFlakeSize = 1;
+var maxFlakeSize = 5;
 
-function draw() {
-	if (mouseIsPressed) {
-		var size = random(10, 100);
-		ellipse(mouseX, mouseY, size, size); 
+var img;
+
+function setup()
+{
+	frameRate(30);
+	noStroke();
+	smooth();
+  img = loadImage("assets/Snow.jpg");
+
+	for(var i = 0; i < quantity; i++)
+	{
+		flakeSize[i] = round(random(minFlakeSize, maxFlakeSize));
+    xPosition[i] = random(0, width);
+    yPosition[i] = random(0, height);
+    direction[i] = round(random(0, 1));
 	}
 }
+
+function draw()
+{
+  image(img, 0, 0);  
+  
+  //background(0);
+
+  for(var i = 0; i < xPosition.length; i++) {
+    
+    ellipse(xPosition[i], yPosition[i], flakeSize[i], flakeSize[i]);
+   
+    if(direction[i] == 0) {
+      xPosition[i] += map(flakeSize[i], minFlakeSize, maxFlakeSize, 0.1, 0.5);
+    } else {
+      xPosition[i] -= map(flakeSize[i], minFlakeSize, maxFlakeSize, 0.1, 0.5);
+    }
+    
+    yPosition[i] += flakeSize[i] + direction[i]; 
+    
+    if(xPosition[i] > width + flakeSize[i] || xPosition[i] < -flakeSize[i] || yPosition[i] > height + flakeSize[i]) {
+      xPosition[i] = random(0, width);
+      yPosition[i] = -flakeSize[i];
+    }
+  }
+}
+
+
+
+
