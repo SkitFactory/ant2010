dcc3d2e0c2fe5a023116dd3234cf91284c5ccb81
SkitFactory <31624504+SkitFactory@users.noreply.github.com>
2017-10-12 16:05:12 +0900

    Update sketch_171010b.js

--- sketch_171010b.js
+++ sketch_171010b.js
@@ -1,50 +1,49 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255, 0, 0);
-  frameRate(24);
+  background(255, 255, 255);
 }
 function draw() {
-  background(255, 0, 0);
-  
-  randomSeed(millis());
+  background(255, 255, 255);
+  randomSeed(0);
   
   strokeWeight(3);
   var x, y;
-  //var delta = map(mouseX, 0, windowWidth, 10, 100);
-  delta = 20;
+  var deltaX = map(mouseX, 0, windowWidth, 10, 500);
+  var deltaY = map(mouseY, 0, windowHeight, 5, 20);
   
-  for (y=0; y<windowHeight; y+=delta) {
-    for (x=0; x<windowWidth; x+=delta) {
-      var r = random(0, 1);
-      if (r < 0.5) {
-        line(x, y, x+delta, y+delta);
-      } else {
-        line(x+delta, y, x, y+delta);
-      }
+  for(var i = 0; i<deltaX; i++)
+  {
+    var r = random(0,1);
+    if(r<0.5)
+    {
+      var rX = random(0, windowWidth);
+      var rY = random(0, windowHeight);
+      line(rX, rY, rX+deltaX, rY+deltaX);
+      stroke(216, 81, 252);
+      strokeWeight(deltaY);
+    }
+    else
+    {
+      var rX = random(0, windowWidth);
+      var rY = random(0, windowHeight);
+      line(rX+deltaX, rY, rX, rY+deltaX);
+      stroke(0, 0, 0);
+      strokeWeight(3);
     }
   }
-  
   /*
-  for (y=0; y<windowHeight; y+=delta) {
-    for (x=0; x<windowWidth; x+=delta) {
-      var r = random(0, 2);
-      if (r < 0.25) {
-        line(x, y, x+delta, y);
-      } else if(r>=0.25&&r<0.5) {
-        line(x, y, x, y+delta);
-      } else if(r>=0.5&&r<0.75) {
-        line(x+delta, y, x, y);
-      } else if(r>=0.75&&r<1.0){
-        line(x, y+delta, x, y);
-      } else if (r>=1.0&&r<1.25) {
-        line(x, y, x-delta, y);
-      } else if(r>=1.25&&r<1.5) {
-        line(x, y, x, y-delta);
-      } else if(r>=1.5&&r<1.75) {
-        line(x-delta, y, x, y);
+  for (y=0; y<windowHeight; y+=deltaX) {
+    for (x=0; x<windowWidth; x+=deltaX) {
+      var r = random(0, 1);
+      if (r < 0.5) {
+        line(x, y, x+deltaX, y+deltaX);
+        stroke(216, 81, 252);
+        strokeWeight(deltaY);
       } else {
-        line(x, y-delta, x, y);
-      } 
+        line(x+deltaX, y, x, y+deltaX);
+        stroke(0, 0, 0);
+        strokeWeight(3);
+      }
     }
   }
   */
