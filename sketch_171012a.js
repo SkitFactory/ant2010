cbbfe4d33043320646f5a2ee08d10c24beeca344
SkitFactory <31624504+SkitFactory@users.noreply.github.com>
2017-10-12 17:36:55 +0900

    Update sketch_171012a.js

--- sketch_171012a.js
+++ sketch_171012a.js
@@ -11,40 +11,19 @@ function draw() {
   var deltaX = map(mouseX, 0, windowWidth, 10, 500);
   var deltaY = map(mouseY, 0, windowHeight, 5, 20);
   
-  for(var i = 0; i<deltaX; i++)
-  {
-    var r = random(0,1);
-    if(r<0.5)
-    {
-      var rX = random(0, windowWidth);
-      var rY = random(0, windowHeight);
-      line(rX, rY, rX+deltaX, rY+deltaX);
-      stroke(216, 81, 252);
-      strokeWeight(deltaY);
-    }
-    else
-    {
-      var rX = random(0, windowWidth);
-      var rY = random(0, windowHeight);
-      line(rX+deltaX, rY, rX, rY+deltaX);
-      stroke(0, 0, 0);
-      strokeWeight(3);
-    }
-  }
-  /*
   for (y=0; y<windowHeight; y+=deltaX) {
     for (x=0; x<windowWidth; x+=deltaX) {
       var r = random(0, 1);
-      if (r < 0.5) {
-        line(x, y, x+deltaX, y+deltaX);
+      if (r < 0.3) { 
         stroke(216, 81, 252);
         strokeWeight(deltaY);
-      } else {
-        line(x+deltaX, y, x, y+deltaX);
+        line(x-deltaX*2, y, x, y+deltaX*2);
+      } else if(r>0.3&&r<0.6) {
         stroke(0, 0, 0);
         strokeWeight(3);
+        line(x+deltaX*2, y, x, y+deltaX*2);
       }
     }
   }
-  */
+  
 }
