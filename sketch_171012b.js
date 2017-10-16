e68e955eb36c2d1d910f891ba87e139be0cb4774
SkitFactory <31624504+SkitFactory@users.noreply.github.com>
2017-10-16 22:06:28 +0900

    Update sketch_171012b.js

--- sketch_171012b.js
+++ sketch_171012b.js
@@ -1,7 +1,10 @@
+var mode = 1;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 0);
 }
+
 function draw() {
   background(0, 0, 0);
   randomSeed(0);
@@ -11,17 +14,58 @@ function draw() {
   var deltaX = map(mouseX, 0, windowWidth, 10, 500);
   var deltaY = map(mouseY, 0, windowHeight, 5, 20);
   
-  for (y=0; y<windowHeight; y+=deltaX) {
-    for (x=0; x<windowWidth; x+=deltaX) {
-      var r = random(0, 1);
-      var alpha = random(0, 255);
-      if (r < 0.5) { 
-        fill(10, 35, 245, alpha);
-        stroke(255, 255, 255);
-        strokeWeight(1);
-        rect(x, y, deltaX, deltaX);
-      } 
+  if(mode == 0)
+  {
+    background(0, 0, 0);
+    for (y=0; y<windowHeight; y+=deltaX) {
+      for (x=0; x<windowWidth; x+=deltaX) {
+        var r = random(0, 1);
+        var alpha = random(0, 255);
+        if (r < 0.5) { 
+          fill(10, 35, 245, alpha);
+          stroke(255, 255, 255);
+          strokeWeight(1);
+          rect(x, y, deltaX, deltaX);
+        } 
+      }
     }
   }
+  else if(mode == 1)
+  {
+    background(0, 0, 0);
+    for (y=0; y<windowHeight; y+=deltaX) {
+      for (x=0; x<windowWidth; x+=deltaX) {
+        var r = random(0, 1);
+        var alpha = random(0, 255);
+        if (r < 0.5) { 
+          fill(10, 35, 245, alpha);
+          stroke(255, 255, 255);
+          strokeWeight(1);
+          ellipse(x+deltaX/2, y+deltaX/2, deltaX, deltaX);
+        } 
+      }
+    }
+  }
+  else if(mode == 2)
+  {
+    background(0, 0, 0);
+  }
   
+}
+
+function keyPressed()
+{
+  if(keyCode == 'q')
+  {
+    mode = 0;
+  }
+  else if(keyCode == 'w')
+  {
+    mode = 1;
+  }
+  else
+  {
+    mode = 2;
+  }
+  return 0;
 }
